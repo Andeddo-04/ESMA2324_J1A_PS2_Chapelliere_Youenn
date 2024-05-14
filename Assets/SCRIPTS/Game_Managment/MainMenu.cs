ef17d954bb5acd324111f9ddac50210fbc12b751
CHAPELLIERE Youenn <youennchapi@gmail.com>
2024-05-14 20:12:18 +0200

    MainMenu
    Récuperation du menu du projet Zelda-Like
    
    menu a remaganner pour l'addapter a une scène

---
+++ Assets/SCRIPTS/Game_Managment/MainMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public static bool gameIsPaused = true;
+
+    public GameObject settingsWindow, canvasMainMenu, canvasUI; //, canvasPauseMenu;
+
+    public PlayerMovement playerMovement;
+
+    //private void Update()
+    //{
+    //    if (canvasMainMenu.activeSelf == true && canvasPauseMenu.activeSelf == false && canvasUI.activeSelf == false)
+    //    {
+    //        Cursor.lockState = CursorLockMode.None;
+    //        Cursor.visible = true;
+    //    }
+    //}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+        canvasMainMenu.SetActive(false);
+        canvasUI.SetActive(true);
+        playerMovement.HideAndLockCursor();
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settingsWindow.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Paused()
+    {
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+}
---
+++ Assets/SCRIPTS/Game_Managment/SwitchControls.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SwitchControls : MonoBehaviour
+{
+    public Toggle controlesClavier, controlesManette;
+
+    public PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        controlesClavier = GameObject.FindGameObjectWithTag("KeyBoardType").GetComponent<Toggle>();
+        controlesManette = GameObject.FindGameObjectWithTag("ControllerType").GetComponent<Toggle>();
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlesClavier.isOn = true;
+        controlesManette.isOn = false; 
+    }
+
+    public void SwitchIntoController()
+    {
+        controlesClavier.isOn = !controlesManette.isOn;
+
+        playerMovement.SetControllerUsage(true);
+    }
+
+    public void SwitchIntoKeyBoard()
+    {
+        controlesManette.isOn = !controlesClavier.isOn;
+
+        playerMovement.SetControllerUsage(false);
+    }
+}
