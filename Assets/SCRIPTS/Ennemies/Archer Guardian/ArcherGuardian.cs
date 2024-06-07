f0cf7b29164f8aa4fe257c52f5712fd32714f0f9
CHAPELLIERE Youenn <youennchapi@gmail.com>
2024-06-07 15:31:49 +0200

    finsh walking animations

--- Assets/SCRIPTS/Ennemies/Archer Guardian/ArcherGuardian.cs
+++ Assets/SCRIPTS/Ennemies/Archer Guardian/ArcherGuardian.cs
@@ -43,16 +43,10 @@ public class ArcherGuardian : MonoBehaviour
             Vector3 direction = target.position - transform.position;
             transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
 
-
-            // Si l'ennemie est quasiment arrivé a sa destination
             if (Vector3.Distance(transform.position, target.position) < 0.3f)
             {
-                desPoint = (desPoint + 1) % waypoints.Length; // % = reste division
+                desPoint = (desPoint + 1) % waypoints.Length;
                 target = waypoints[desPoint];
-                mobSpriteRenderer.flipX = !mobSpriteRenderer.flipX;
-
-                //isFacingRight devient son opposé (TRUE ou FALSE)
-                //on multiplie le scale X du joueur par -1
                 isFacingRight = !isFacingRight;
                 Vector3 localScale = transform.localScale;
                 localScale.x *= -1f;
